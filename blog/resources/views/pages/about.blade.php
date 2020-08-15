6f43785272e956e22fa24f36ae9eaaa34ec88020
Jenniline <jennilineebai@gmail.com>
2020-08-15 10:07:22 +0100

    test change on about page after installing on new vscode on Elementary

--- blog/resources/views/pages/about.blade.php
+++ blog/resources/views/pages/about.blade.php
@@ -8,7 +8,8 @@
         <div class="col-md-12">
             <h1>About Me</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dolorum aliquam consectetur sapiente voluptatum officiis inventore, 
-            aspernatur aut vel qui voluptatem distinctio maiores est repudiandae unde perferendis consequatur quod similique.</p> 
+            aspernatur aut vel qui voluptatem distinctio maiores est repudiandae unde perferendis consequatur quod similique. Adding a test change after clonning on vscode Elementary
+        </p> 
 
         </div>
     </div>
